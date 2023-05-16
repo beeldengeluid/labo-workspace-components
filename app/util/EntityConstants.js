328674a3f26ad3f9d7264f21fb0e95b5996c30aa
Jaap Blom <jblom@beeldengeluid.nl>
2023-05-16 14:49:45 +0200

    removing unused js

--- app/util/EntityConstants.js
+++
@@ -1,9 +0,0 @@
-export const PERSON = "person";
-export const LOCATION = "location";
-export const ORGANISATION = "organisation";
-
-export const ENTITY_TYPE = {
-  PERSON,
-  LOCATION,
-  ORGANISATION,
-};
--- app/util/IconUtil.js
+++ app/util/IconUtil.js
@@ -1,24 +1,4 @@
 const IconUtil = {
-  //TODO somehow also allow recipes to override icons?
-  getAnnotationTemplateIcon(template, border, muted, interactive) {
-    let iconClass = "fas fa-question";
-    if (template === "av" || template === "video") {
-      iconClass = "fas fa-film";
-    } else if (template === "audio") {
-      iconClass = "fas fa-signal";
-    } else if (template === "artwork") {
-      iconClass = "fas fa-paint-brush";
-    } else if (template === "link") {
-      iconClass = "fas fa-link";
-    } else if (template === "image") {
-      iconClass = "far fa-image";
-    } else if (template === "article" || template === "text") {
-      iconClass = "far fa-file-alt";
-    } else if (template === "person") {
-      iconClass = "fas fa-user-circle";
-    }
-    return IconUtil.__addExtraStyling(iconClass, border, muted, interactive);
-  },
 
   getMimeTypeIcon(mimeType, border, muted, interactive) {
     let iconClass = "";
@@ -34,34 +14,6 @@ const IconUtil = {
       } else if (mimeType.indexOf("text") !== -1) {
         iconClass = "far fa-file-alt";
       }
-    }
-    return IconUtil.__addExtraStyling(iconClass, border, muted, interactive);
-  },
-
-  getUserActionIcon(action, border, muted, interactive) {
-    let iconClass = "fas fa-question";
-    if (action.indexOf("save") !== -1) {
-      iconClass = "fas fa-save";
-    } else if (action.indexOf("remove") !== -1) {
-      iconClass = "fas fa-times";
-    } else if (action.indexOf("add") !== -1) {
-      iconClass = "fas fa-plus";
-    } else if (action.indexOf("annotate") !== -1) {
-      iconClass = "fas fa-sticky-note";
-    } else if (action.indexOf("next") !== -1) {
-      iconClass = "fas fa-caret-right";
-    } else if (action.indexOf("previous") !== -1) {
-      iconClass = "fas fa-caret-left";
-    } else if (action.indexOf("play") !== -1) {
-      iconClass = "fas fa-play";
-    } else if (action.indexOf("comment") !== -1) {
-      iconClass = "fas fa-comment";
-    } else if (action.indexOf("link") !== -1) {
-      iconClass = "fas fa-link";
-    } else if (action.indexOf("classification") !== -1) {
-      iconClass = "fas fa-tag";
-    } else if (action.indexOf("metadata") !== -1) {
-      iconClass = "fas fa-sticky-note";
     }
     return IconUtil.__addExtraStyling(iconClass, border, muted, interactive);
   },
